aa979ee0e2be1b538f525d1b154778ed5d054ccc
Lakshya Singh <lakshya.singh354@gmail.com>
2025-09-02 12:44:59 +0000

    matmul tiled

--- matmul.cu
+++ matmul.cu
@@ -1,6 +1,8 @@
 #include <cuda_runtime.h>
 #include <iostream>
 
+#define TILE_SIZE 2
+
 __global__ void matmul_naive(float* A, float* B, float* C, int m, int k, int n){
     int row = blockDim.y * blockIdx.y + threadIdx.y;
     int col = blockDim.x * blockIdx.x + threadIdx.x;
@@ -14,7 +16,45 @@ __global__ void matmul_naive(float* A, float* B, float* C, int m, int k, int n){
     }
 }
 
+__global__ void matmul_tiled(float* A, float* B, float* C, int m, int k, int n){
+    int row = blockIdx.y * TILE_SIZE + threadIdx.y;
+    int col = blockIdx.x * TILE_SIZE + threadIdx.x;
 
+    __shared__ float As[TILE_SIZE][TILE_SIZE];
+    __shared__ float Bs[TILE_SIZE][TILE_SIZE];
+
+    float sum = 0.0f;
+    for(int t = 0; t < (k + TILE_SIZE - 1)/TILE_SIZE; ++t){
+        int Arow = row;
+        int Acol = t * TILE_SIZE + threadIdx.x;
+        int Brow = t * TILE_SIZE + threadIdx.y;
+        int Bcol = col;
+
+        if (Arow < m && Acol < k){
+            As[threadIdx.y][threadIdx.x] = A[Arow * k + Acol];
+        } else{
+            As[threadIdx.y][threadIdx.x] = 0.0f;
+        }
+
+        if (Brow < k && Bcol < n){
+            Bs[threadIdx.y][threadIdx.x] = B[Brow * n + Bcol];
+        } else{
+            Bs[threadIdx.y][threadIdx.x] = 0.0f;
+        }
+
+        __syncthreads();
+
+        for(int p = 0; p < k; p++){
+            sum += As[threadIdx.y][p] * Bs[p][threadIdx.x];
+        }
+        __syncthreads();
+    }
+
+    if(row < m && col < n){
+        C[row * n + col] = sum;
+    }
+
+}
 
 int main(){
     int m = 4, k = 4, n = 4;
@@ -67,10 +107,10 @@ int main(){
     cudaMemcpy(d_A, h_A, size_A, cudaMemcpyHostToDevice);
     cudaMemcpy(d_B, h_B, size_B, cudaMemcpyHostToDevice);
 
-    dim3 threadsPerBlock(16, 16);
-    dim3 numBlocks((m + 15)/16, (n + 15)/16);
+    dim3 threadsPerBlock(TILE_SIZE, TILE_SIZE);
+    dim3 numBlocks((n + TILE_SIZE - 1)/TILE_SIZE, (m + TILE_SIZE - 1)/TILE_SIZE);
 
-    matmul_naive<<<numBlocks, threadsPerBlock>>>(d_A, d_B, d_C, m, k, n);
+    matmul_tiled<<<numBlocks, threadsPerBlock>>>(d_A, d_B, d_C, m, k, n);
 
     cudaMemcpy(h_C, d_C, size_C, cudaMemcpyDeviceToHost);
 
