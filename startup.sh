848b55793dad4a0645da50237a5ed141439411c8
LakshyaSingh354 <lakshya.singh354@gmail.com>
2025-09-07 02:41:44 +0530

    refactor startup script

--- startup.sh
+++ startup.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-keyring_1.1-1_all.deb
 sudo dpkg -i cuda-keyring_1.1-1_all.deb
 sudo apt-get update
@@ -8,8 +10,10 @@ source ~/.bashrc
 
 pip install --index-url https://download.pytorch.org/whl/cu124 torch torchvision torchaudio
 git clone https://github.com/LakshyaSingh354/CudaCode.git
-git clone https://github.com/LakshyaSingh354/Inferno.git
-cd Inferno
-python3 -m venv .venv
-source .venv/bin/activate
-pip install -r requirements.txt
+# git clone https://github.com/LakshyaSingh354/Inferno.git
+# cd Inferno
+# python3 -m venv .venv
+# source .venv/bin/activate
+# pip install -r requirements.txt
+git config --global user.email "lakshya.singh354@gmail.com"
+git config --global user.name "Lakshya Singh"
